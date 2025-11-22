9fb634b6553ef176048882aab9147dad9df0408c
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-23 01:11:09 +0300

    create reusable Button component with sizing and shape variants

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,19 +6,23 @@ const Landing: React.FC =  () => {
     <div className="p-4">
       <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
       
+      {/* Your Existing Cards */}
       <div className="mb-8">
         <Card /> <Card /> <Card />
       </div>
 
-      {/* New Button Instances */}
-      <div className="flex gap-4 items-center">
+      {/* Button Instances */}
+      <div className="flex gap-4 items-center flex-wrap">
         {/* Small - Rounded Small */}
         <Button title="Small Button" styles="text-sm rounded-sm" />
         
         {/* Medium - Rounded Medium */}
         <Button title="Medium Button" styles="text-base rounded-md" />
         
-        {/* Large - Rounded Large */}
+        {/* New: Rounded Large */}
+        <Button title="Rounded LG Button" styles="text-base rounded-lg" />
+
+        {/* Large - Rounded Full */}
         <Button title="Large Button" styles="text-lg rounded-full" />
       </div>
     </div>
@@ -26,4 +30,3 @@ const Landing: React.FC =  () => {
 }
 
 export default Landing;
-
