eff413e3e3e6b290c143368720169faf10051448
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-23 00:55:26 +0300

    create reusable components with dynamic props support

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -4,7 +4,7 @@ const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card />
+      <Card /> <Card />
     </div>
   )
 }
